a1a447db2669f42916fea2610b75965d02177302
Gregor Adams <greg@pixelass.com>
2019-11-01 19:32:21 +0100

    chore: transform cloudinary images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,84 +1,101 @@
+// Load the environment variables.
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
-    description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-typescript",
-    "gatsby-plugin-styled-components",
-    {
-      resolve: "gatsby-plugin-transition-link",
-      options: {
-        layout: require.resolve(`${__dirname}/src/layout/index.tsx`)
-      }
-    },
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/static/img`,
-        name: "uploads"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: "pages"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/static/fonts`,
-        name: "fonts"
-      }
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-relative-images",
-            options: {
-              name: "uploads"
-            }
-          },
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048
-            }
-          },
-          {
-            resolve: "gatsby-remark-copy-linked-files",
-            options: {
-              destinationDir: "static"
-            }
-          }
-        ]
-      }
-    },
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
-      }
-    },
-    "gatsby-plugin-netlify" // make sure to keep it last in the array
-  ]
+	siteMetadata: {
+		title: "Gatsby + Netlify CMS Starter",
+		description:
+			"This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
+	},
+	plugins: [
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-typescript",
+		"gatsby-plugin-styled-components",
+		{
+			resolve: "gatsby-plugin-transition-link",
+			options: {
+				layout: require.resolve(`${__dirname}/src/layout/index.tsx`)
+			}
+		},
+		{
+			// keep as first gatsby-source-filesystem plugin for gatsby image support
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/static/img`,
+				name: "uploads"
+			}
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/pages`,
+				name: "pages"
+			}
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/img`,
+				name: "images"
+			}
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/static/fonts`,
+				name: "fonts"
+			}
+		},
+
+		{
+			resolve: "gatsby-transformer-cloudinary",
+			options: {
+				cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+				apiKey: process.env.CLOUDINARY_API_KEY,
+				apiSecret: process.env.CLOUDINARY_API_SECRET,
+
+				// This folder will be created if it doesn’t exist.
+				uploadFolder: "gatsby-cloudinary"
+			}
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-transformer-remark",
+			options: {
+				plugins: [
+					{
+						resolve: "gatsby-remark-relative-images",
+						options: {
+							name: "uploads"
+						}
+					},
+					{
+						resolve: "gatsby-remark-images",
+						options: {
+							// It's important to specify the maxWidth (in pixels) of
+							// the content container as this plugin uses this as the
+							// base for generating different widths of each image.
+							maxWidth: 2048
+						}
+					},
+					{
+						resolve: "gatsby-remark-copy-linked-files",
+						options: {
+							destinationDir: "static"
+						}
+					}
+				]
+			}
+		},
+		{
+			resolve: "gatsby-plugin-netlify-cms",
+			options: {
+				modulePath: `${__dirname}/src/cms/cms.js`
+			}
+		},
+		"gatsby-plugin-netlify" // make sure to keep it last in the array
+	]
 };
